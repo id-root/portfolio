84062969b87616b61d37fba131773628cf09355d
AGI <248829238+id-root@users.noreply.github.com>
2026-05-13 15:29:00 +0530

    Update Content-Security-Policy in next.config.js

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
                 headers: [
                     {
                         key: 'Content-Security-Policy',
-                        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.spline.design https://unpkg.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: https://*.spline.design; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://*.spline.design https://unpkg.com; frame-src 'self' https://*.spline.design; worker-src 'self' blob:;",
+                        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.spline.design https://unpkg.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: https://*.spline.design https://cdn.simpleicons.org; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://*.spline.design https://unpkg.com; frame-src 'self' https://*.spline.design; worker-src 'self' blob:;",
                     },
                     {
                         key: 'X-Frame-Options',
