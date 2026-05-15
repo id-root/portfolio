bc4456c7a165fedc86d4aa16a3142bfec8a06210
AGI <248829238+id-root@users.noreply.github.com>
2026-05-15 11:01:43 +0530

    Delete src directory

--- src/components/doodles/index.ts
+++
@@ -1,6 +0,0 @@
-export { ScribbleUnderline } from "./ScribbleUnderline";
-export { StarSparkle } from "./StarSparkle";
-export { CurlyArrow } from "./CurlyArrow";
-export { LoopCircle } from "./LoopCircle";
-export { SquiggleDivider } from "./SquiggleDivider";
-export { LibraryBookIllustration } from "./LibraryBookIllustration";
